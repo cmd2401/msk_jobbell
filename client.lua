370765397510a295af3deba1ad736c9e3e770994
cmd <96431684+cmd2401@users.noreply.github.com>
2022-10-12 21:57:15 +0200

    cl

---
+++ client.lua
@@ -0,0 +1,27 @@
+ESX = exports['es_extended']:getSharedObject()
+
+Citizen.CreateThread(function()
+    while true do
+        local wait = 500
+    
+        for _,v in pairs(Config.Position) do
+            local pos = v.pos
+            local pPos = GetEntityCoords(PlayerPedId())
+            local dst = Vdist(pPos.x, pPos.y, pPos.z, pos.x, pos.y, pos.z)
+
+            if dst <= 3.0 then
+                wait = 0
+                DrawMarker(Config.MarkerType, pos.x, pos.y, (pos.z)-1.0,0.0, 0.0, 0.0, 360.0, 0.0, 0.0, 1.0, 1.0, 1.0, 60, 66, 207, 155)
+            end
+            if dst <= 1.0 then
+                wait = 0
+                ESX.ShowHelpNotification(Config.ShowHelpNotification)
+                if IsControlJustPressed(1, Config.Control) then
+                    TriggerServerEvent("bell:notify", v.job)
+                end
+            end
+        end
+
+        Citizen.Wait(wait)
+    end
+end)
