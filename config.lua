8989fc474e1656628d9a4ed01523aac8325b31d4
cmd <96431684+cmd2401@users.noreply.github.com>
2022-10-16 23:16:11 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 Config = {
     Control = 51, -- E
-    ShowHelpNotification = "~INPUT_CONTEXT~ Klingel betätigen",
-    WaitAfterBell = 10000,
+    ShowHelpNotification = "~INPUT_CONTEXT~ Klingel betätigen", -- ring the bell notification
+    WaitAfterBell = 10000, -- 10 seconds
     Position = {
         {pos = vector3(438.74417114258, -987.93365478516, 30.724325180054), job = "police"},
         {pos = vector3(440.29974365234, -984.40850830078, 30.724325180054), job = "police"},
