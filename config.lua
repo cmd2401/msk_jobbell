e70d9dae2c8b0f09e4d1f75cf4b3c51f56d5c660
cmd <96431684+cmd2401@users.noreply.github.com>
2022-10-16 23:12:04 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {
     Control = 51, -- E
-    ShowHelpNotification = "~INPUT_CONTEXT~ Klingel betätigen"
+    ShowHelpNotification = "~INPUT_CONTEXT~ Klingel betätigen",
     Position = {
         {pos = vector3(438.74417114258, -987.93365478516, 30.724325180054), job = "police"},
         {pos = vector3(440.29974365234, -984.40850830078, 30.724325180054), job = "police"},
