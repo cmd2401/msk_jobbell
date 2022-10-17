6e6cc5206f12b3d38ce8238af0761ab111ad75cd
cmd <96431684+cmd2401@users.noreply.github.com>
2022-10-17 20:54:04 +0200

    fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+games {'gta5'}
+author 'MSK Scripts'
+version '1.2'
+
+shared_scripts { "config.lua" }
+
+client_scripts {
+    "client.lua",
+}
+
+server_scripts {
+    "@mysql-async/lib/MySQL.lua",
+    "server.lua",
+}
